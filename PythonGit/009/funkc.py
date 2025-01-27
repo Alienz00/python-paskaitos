ce3813b39a39868f5a0601fb017c8f7320fd6ec5
Vytautas Rakauskas <vyt.rakauskas@gmail.com>
2025-01-27 20:50:10 +0200

    pridetos dar 2 uzduotys trecia ir ketvirta

--- PythonGit/009/funkc.py
+++ PythonGit/009/funkc.py
@@ -40,3 +40,89 @@ def sudaugink(x, y):
 
 rezultatas = sudaugink(x, y)
 print(rezultatas)
+
+# destytojo
+
+def daugink(x, y):
+    if not x or not y:
+        return
+    return x * y
+
+suma = daugink(5, 10)
+if suma:
+    print(suma)
+else:
+    print('Netinkami skaiciai')
+
+# pasveikins 2 ozius
+def say_hi_to_two(name1, name2):
+    if not name1 or not name2:
+        return
+    hello_string = f'Hello to {name1} and hello to {name2}'
+    return hello_string
+pasveikinimas = say_hi_to_two('Petras', 'Kazys')
+print(pasveikinimas)
+
+
+# ives teksta sutrumpintai
+def greet(name='Jimm'):
+    print(f'Hello, {name}!')
+
+greet('Vytautas')
+greet()
+
+
+# argumentai, kuriems butinai reikia perduoti reiksme
+
+def priimk_pacienta(ligonis, gydytojas='gyd. Pagalnutylenis'):
+    irasas_gyd_zurnale = f'Pacientas {ligonis}, prieme {gydytojas}'
+    return irasas_gyd_zurnale
+
+res = priimk_pacienta('Adomas')
+print(res)
+print('...........')
+
+res = priimk_pacienta('Kazys')
+print(res)
+print('...........')
+
+res = priimk_pacienta('Petras', 'gyd. Pakaitenis')
+print(res)
+print('...........')
+
+
+# Sukurkite funkciją trys_sveikinimai(vardas1, vardas2, vardas3),
+# kuri priimtų tris
+# vardus ir kiekvienam iš jų atspausdintų pasisveikinimą.
+# Pvz.: „Labas, Jonas!“, „Labas, Asta!“, „Labas, Mantas!“
+
+def trys_pasveikinimai(vardas1, vardas2, vardas3):
+    if not vardas1 or not vardas2 or not vardas3:
+        return
+    sveikinu = f'Labas, {vardas1}, Labas {vardas2} ir labas {vardas3}'
+    return sveikinu
+
+pasisveikink = trys_pasveikinimai('Jonas', 'Asta', 'Mantas')
+print(pasisveikink)
+
+
+# Numatytosios reikšmės ir keyword argumentai
+# Užduotis 4:
+# Sukurkite funkciją sveikink_su_pavadinimu(vardas, pavadinimas="drauge"), kuri
+# atspausdintų žinutę: „Sveikas, [vardas]! Ką veiki, [pavadinimas]?“.
+# 1. Iškvieskite funkciją nenurodydami pavadinimo.
+# 2. Iškvieskite funkciją, nurodydami pavadinimą „kolega“.
+
+pavadinimas = 'drauge'
+def sveikink_su_pavadinimu(vardas, pavadinimas):
+    if not vardas or not pavadinimas:
+        return
+    zinute = f'Sveikas, {vardas}! Ka veiki, {pavadinimas}'
+    return zinute
+
+be_pav = sveikink_su_pavadinimu('Petras', pavadinimas)
+print(be_pav)
+
+su_pav = sveikink_su_pavadinimu('Kazys', 'Kolega')
+print(su_pav)
+
