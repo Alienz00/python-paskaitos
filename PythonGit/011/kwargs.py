adf1cc8834ff7296703afb8f74352e959cb8737a
Vytautas Rakauskas <vyt.rakauskas@gmail.com>
2025-01-29 20:39:24 +0200

    paskaitos 011 darbai nr 2

---
+++ PythonGit/011/kwargs.py
@@ -0,0 +1,39 @@
+
+def print_kwargs(**kwargs):
+    print(kwargs)
+    print(kwargs['pirmas'])
+    print(type(kwargs))
+
+print_kwargs(pirmas=3, antras=2)
+
+
+def print_list(listas, skirtukas=' ', eilutes_pabaiga='\n'):
+    for elem in listas:
+        print(elem, 'men.', sep=skirtukas, end=eilutes_pabaiga)
+
+listas_duom = ['sausio', 'vasario', 'kovo']
+print_list(listas_duom)
+print('..................')
+print_list(listas_duom, skirtukas='|||', eilutes_pabaiga='***\n')
+
+
+# o cia naudojant kwargs
+
+def print_lyst(listas, **kwargs):
+    for elem in listas:
+        print(elem, 'men', **kwargs)
+listas_duom = ['sausio', 'vasario', 'kovo']
+print_lyst(listas_duom, sep='>>>', end='<<<\n')
+
+
+# Įvadas į **kwargs
+#
+# Užduotis 7:
+# Parašykite funkciją rodyti_duomenis(**kwargs), kuri išspausdintų visus perduotus
+# vardinius argumentus raktu ir reikšme.
+
+def muzika(stiliai, **kwargs):
+    for stilius in stiliai:
+        print(stilius, 'muzika', **kwargs)
+stiliu_sarasas = ['Techno', 'Jungle', 'Psychedelic trance']
+muzika(f'>>>, {stiliu_sarasas}, end=<<<\n')
